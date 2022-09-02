f9d8c3410e985d49148eebecd2e822e58a82adab
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-09-02 16:35:22 +0530

    Add files via upload

---
+++ ProgramE/ProgramE/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramE
+{
+    class Program
+    {
+        private int x = 10;
+        private int y = 100;
+        public void show()
+        {
+            Console.WriteLine(x + y);
+        }
+        public void display()
+        {
+            Console.WriteLine(x - y);
+        }
+    }
+    class Demo { 
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.show();
+            p.display();
+
+            Console.WriteLine("sum: "+ (p.x + p.y));
+            Console.WriteLine("sub: "+(p.x- p.y));
+            Console.ReadLine();
+        }
+    }
+}
